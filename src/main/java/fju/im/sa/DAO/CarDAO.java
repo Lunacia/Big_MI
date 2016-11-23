e748f8317a57ca4ffe3e2e639e1d7bce40fc766d
Hsueh-YANG <prettyjim841231@gmail.com>
2016-11-23 22:28:22 +0800

    DAO increase

--- src/main/java/fju/im/sa/DAO/CarDAO.java
+++ src/main/java/fju/im/sa/DAO/CarDAO.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import fju.im.sa.entity.Car;
 public interface CarDAO {
-
+    
+	public void deleteAll(Car car);
+	public void delete(int Product_Amount);
+	public void add(int Product_Amount);
+	public void insert(Car car);
 	public List<Car> getList();
 }
