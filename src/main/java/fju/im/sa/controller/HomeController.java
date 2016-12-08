89d333a62a58a1f5c6ef82b73ab996d62eaa3485
lawrencelin26 <lawrencelin26@gmail.com>
2016-12-08 11:07:21 +0800

    homecontroller

--- src/main/java/fju/im/sa/controller/HomeController.java
+++ src/main/java/fju/im/sa/controller/HomeController.java
@@ -1,4 +1,4 @@
-package fju.im.sa.controller;
+ package fju.im.sa.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -18,17 +18,63 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	
+	
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home(String name) {
-		 ModelAndView model1 = new ModelAndView("index");
+	public ModelAndView home() {
+		 ModelAndView model1 = new ModelAndView("user/index");
 		 return model1;
 	}
+
+	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView LoginPage(String name) {
-		ModelAndView model1 = new ModelAndView("login");
+		ModelAndView model1 = new ModelAndView("user/login");
 		 return model1;
 	}
+	@RequestMapping(value = "/member", method = RequestMethod.GET)
+	public ModelAndView MemberPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/member");
+		 return model1;
+	}
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public ModelAndView SignupPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/signup");
+		 return model1;
+	}
+	@RequestMapping(value = "/order", method = RequestMethod.GET)
+	public ModelAndView OrderPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/order");
+		 return model1;
+	}
+	@RequestMapping(value = "/memberEdit", method = RequestMethod.GET)
+	public ModelAndView MemberEditPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/memberEdit");
+		 return model1;
+	}
+	@RequestMapping(value = "/cart", method = RequestMethod.GET)
+	public ModelAndView CartPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/cart");
+		 return model1;
+	}
+	@RequestMapping(value = "/serviceP", method = RequestMethod.GET)
+	public ModelAndView ServicepPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/serviceP");
+		 return model1;
+	}
+	@RequestMapping(value = "/contact", method = RequestMethod.GET)
+	public ModelAndView ContactPage(String name) {
+		ModelAndView model1 = new ModelAndView("user/contact");
+		 return model1;
+	}
+	//@RequestMapping(value = "/productList/", method = RequestMethod.GET)
+	//public ModelAndView ProductApplePage(String name) {
+		//ModelAndView model1 = new ModelAndView("productList");
+		 //return model1;
+	//}
+
+
 }
